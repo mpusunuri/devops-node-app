ecf7fa8ad5415544350402d29ff0294e6e2d21dd
mpusunuri <mpusunuri@gmail.com>
2026-02-28 14:06:15 +0530

    Test webhook auto-trigger

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps Task Running");
+  res.send("DevOps Task Running sucerssfully!");
 });
 
 const PORT = 3000;
